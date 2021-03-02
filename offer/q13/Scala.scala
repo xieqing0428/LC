4f31b30195ae553152dca273f40350852022ee95
alessa0 <fm98.7@qq.com>
2021-03-02 19:10:26 +0800

    UPDATE OFFER/Q13

--- offer/q13/Scala.scala
+++ offer/q13/Scala.scala
@@ -1,11 +1,29 @@
 package cn.alessa0.lc.offer.q13
 
 /**
+ * 剑指 Offer 13. 机器人的运动范围
+ * <p>
+ * 地上有一个m行n列的方格，从坐标 [0,0] 到坐标 [m-1,n-1] 。
+ * 一个机器人从坐标 [0, 0]的格子开始移动，它每次可以向左、右、上、下移动一格（不能移动到方格外），也不能进入行坐标和列坐标的数位之和大于k的格子。
+ * <p>
+ * 例如，当k为18时，机器人能够进入方格 [35, 37] ，因为3+5+3+7=18。
+ * 但它不能进入方格[35, 38]，因为3+5+3+8=19。
+ * 请问该机器人能够到达多少个格子？
+ *
  * @author Alessa0
  * @since 2021/3/2 18:33
  * @version 0.0.1
  */
 object Scala {
+  /**
+   * Time  O(mn)
+   * Space O(mn)
+   *
+   * @param m 横坐标
+   * @param n 纵坐标
+   * @param k 总数
+   * @return 结果
+   */
   def movingCount(m: Int, n: Int, k: Int): Int = {
     val visited = Array.fill(m + 1, n + 1)(false)
     dfs(visited, m, n, k, 0, 0)
