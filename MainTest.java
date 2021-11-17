a3a77edcaf102224d2f0156eccd1a2ad0a8e4b75
听泉么么哒 <fm98.7@qq.com>
2021-11-17 13:09:09 +0800

    ADD MainTest.java

---
+++ MainTest.java
@@ -0,0 +1,12 @@
+package cn.alessa0.lc;
+
+/**
+ * @author Alessa0
+ * @version 0.0.1
+ * @since 2021/11/17 13:05
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
